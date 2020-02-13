7fb54ab0dde50f696fd23b38d12aeb8d596e3581
ektaindo <ekta.indo@gmail.com>
2020-02-13 19:27:45 +0530

    student record

--- studentdata.js
+++ studentdata.js
@@ -8,7 +8,7 @@ async function viewStudentRecordTable(){
     console.log(stdData);
     let fee = stdData.stdFee
     stdt = stdt + fee;
-    stdtrs += "<tr><td>"+ stdData.stdName +"</td><td>"+ stdData.stdFName+"</td><td>"+stdData.StdMName+"</td><td>"+stdData.stdFee+"</td><td>"+stdData.stdDob+"</td><td>"+stdData.stdCorse+"</td></tr>"
+    stdtrs += "<tr><td>"+ stdData.stdName +"</td><td>"+ stdData.stdFName+"</td><td>"+stdData.StdMName+"</td><td>"+stdData.stdFee+"</td><td>"+stdData.stdDob+"</td><td>"+stdData.stdCourse+"</td></tr>"
   }
   let studentTemplate = "<table><tr><th>Student Name</th><th>Student Father's Name</th><th>Student Mother's Name</th><th>Student Fee</th><th>Student DOB</th><th>Student Course</th></tr>" + stdtrs + "</table>"
   viewStdtable.innerHTML = studentTemplate
@@ -28,38 +28,44 @@ async function pickStdInputValue(){
   let stdMName = stdMElem.value
   console.log("StudentMother'sName",stdMName);
   let stdFeeElem = document.getElementById("Stddemo4")
-  let stdFee = stdFeeElem.value
+  let stdFee = parseInt(stdFeeElem.value)
   console.log("StudentFee",stdFee);
-  let stdDobElem = document.getElementById("Stddemo5")
+  let stdDobElem = document.getElementById("stddemo5")
   let stdDob = stdDobElem.value
   console.log("StudentDOB",stdDob);
   let stdCourseElem = document.getElementById("stddrpbtn")
-  let stdCorse = stdCourseElem.value
+  let stdCourse = stdCourseElem.value
   console.log("StudentCourse",stdCourse);
 
-  if (stdName == "" || stdFName == "" || stdMName == "" || stdFee == "0" || stdDob == "" || stdDob=="" || stdCorse=="") {
+  if (stdName == "" || stdFName == "" || stdMName == "" || stdFee == "0" || stdDob == "" || stdCourse=="") {
     alert("Please fill all details")
     return;
   }
 
-  let studentRecordrow =  {stdName, stdFName,stdMName, stdFee, stdDob, stdCorse}
+  let studentRecordrow =  {stdName, stdFName,stdMName, stdFee, stdDob, stdCourse}
 
   let insert = await firebaseWrapper.insert('studentRecord', studentRecordrow)
   viewStudentRecordTable()
 
 }
- function resetStudentRecord (){
+ function resetSTDRecord(){
    let stdElem = document.getElementById('Stddemo1')
    stdElem.value = ""
    console.log(stdElem.value);
    let stdFElem= document.getElementById("Stddemo2")
    stdFElem.value=""
    console.log(stdFElem.value);
-   let elem3= document.getElementById("Stddemo3")
-   elem3.value=""
-   console.log(elem3.value);
+   let stdMelem3= document.getElementById("Stddemo3")
+   stdMelem3.value=""
+   console.log(stdMelem3.value);
    let elem4= document.getElementById("Stddemo4")
    elem4.value=""
    console.log(elem4.value);
+   let elem5= document.getElementById("stddemo5")
+   elem5.value=""
+   console.log(elem5.value);
+   let elem6= document.getElementById("stddrpbtn")
+   elem6.value=""
+   console.log(elem6.value);
  }
 viewStudentRecordTable();
